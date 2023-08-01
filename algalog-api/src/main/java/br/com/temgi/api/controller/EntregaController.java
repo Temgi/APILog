288721e4c50c96fc0dbb6a8df28af6e83170b0ff
Rui Temgi <ruisaraivasp@gmail.com>
2023-08-01 12:40:39 -0300

    Validacao em Cascata e Validation Groups

--- algalog-api/src/main/java/br/com/temgi/api/controller/EntregaController.java
+++ algalog-api/src/main/java/br/com/temgi/api/controller/EntregaController.java
@@ -1,6 +1,13 @@
 package br.com.temgi.api.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.temgi.domain.model.Entrega;
+import br.com.temgi.domain.repository.EntregaRepository;
 import br.com.temgi.domain.service.SolicitacaoEntregaService;
 import lombok.AllArgsConstructor;
 
@@ -16,11 +24,24 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/entregas")
 public class EntregaController {
 	
+	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitaEntregaService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@RequestBody Entrega entrega) {
+	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 		return solicitaEntregaService.solicitar(entrega); 
+	}
+	
+	@GetMapping
+	public List<Entrega> listar(){
+		return entregaRepository.findAll();
+	}
+	
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId){
+		return entregaRepository.findById(entregaId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
