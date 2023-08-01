127717d7d21e7e1f427e730dcd1a4481f92712bf
Rui Temgi <ruisaraivasp@gmail.com>
2023-08-01 12:31:10 -0300

    Aplicando Jakarta Persistence JPA

--- algalog-api/src/main/java/br/com/temgi/api/controller/ClienteController.java
+++ algalog-api/src/main/java/br/com/temgi/api/controller/ClienteController.java
@@ -2,22 +2,24 @@ package br.com.temgi.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.temgi.domain.model.Cliente;
+import br.com.temgi.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
-	
-	@PersistenceContext
-	private EntityManager manager;
-	
+
+//	@Autowired
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+//		return clienteRepository.findAll();
+//		return clienteRepository.findByNome("Maria Abadia");
+		return clienteRepository.findByNomeContaining("a");
 	}
 }
