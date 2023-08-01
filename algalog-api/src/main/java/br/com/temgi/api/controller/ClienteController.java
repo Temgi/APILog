fff32e790d729da09b5664a63d3bb9960825046a
Rui Temgi <ruisaraivasp@gmail.com>
2023-08-01 12:27:11 -0300

    Configurando o Flyway no Projeto

--- algalog-api/src/main/java/br/com/temgi/api/controller/ClienteController.java
+++ algalog-api/src/main/java/br/com/temgi/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.temgi.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,26 +13,11 @@ import br.com.temgi.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
-	@GetMapping("/testes")
-	public String listaAPI() {
-		return "Teste API";
-	}
+	@PersistenceContext
+	private EntityManager manager;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("11-9999-7788");
-		cliente1.setEmail("joao@temgi.com.br");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("11-9999-3355");
-		cliente2.setEmail("maria@temgi.com.br");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
